55c095f8430acc93551495ad747716fb56a4f5d9
zaxxic <renodzuhri@gmail.com>
2023-08-29 13:53:34 +0700

    tampilan di user bagian profile

--- resources/views/layouts1/header.blade.php
+++ resources/views/layouts1/header.blade.php
@@ -43,7 +43,7 @@
               <a class="nav-link pe-0" href="javascript:void(0)" id="drop1" data-bs-toggle="dropdown" aria-expanded="false">
                 <div class="d-flex align-items-center">
                   <div class="user-profile-img">
-                    <img src="../../dist/images/profile/user-1.jpg" class="rounded-circle" width="35" height="35" alt="" />
+                    <img src="assets/dist/images/profile/user-1.jpg" class="rounded-circle" width="35" height="35" alt="" />
                   </div>
                 </div>
               </a>
@@ -53,7 +53,7 @@
                     <h5 class="mb-0 fs-5 fw-semibold">User Profile</h5>
                   </div>
                   <div class="d-flex align-items-center py-9 mx-7 border-bottom">
-                    <img src="../../dist/images/profile/user-1.jpg" class="rounded-circle" width="80" height="80" alt="" />
+                    <img src="assets/dist/images/profile/user-1.jpg" class="rounded-circle" width="80" height="80" alt="" />
                     <div class="ms-3">
                       <h5 class="mb-1 fs-3">Mathew Anderson</h5>
                       <span class="mb-1 d-block text-dark">Designer</span>
