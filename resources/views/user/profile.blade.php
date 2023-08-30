bd3cfed64dfdda929c5ff4809ba56483b8870d99
zaxxic <renodzuhri@gmail.com>
2023-08-30 10:10:16 +0700

    benerin

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,111 +4,132 @@
         <div class="tab-content" id="pills-tabContent">
             <div class="tab-pane fade show active" id="pills-account" role="tabpanel" aria-labelledby="pills-account-tab"
                 tabindex="0">
+
                 <div class="row">
                     <div class="col-lg-6 d-flex align-items-stretch">
                         <div class="card w-100 position-relative overflow-hidden">
                             <div class="card-body p-4">
-                                <h5 class="card-title fw-semibold">Ganti Profile</h5>
-                                <p class="card-subtitle mb-4">Ganti Photo Ptofile mu disini</p>
-                                <div class="text-center">
-                                    <img src="assets/dist/images/profile/user-1.jpg" alt=""
-                                        class="img-fluid rounded-circle" width="120" height="120">
-                                    <div class="d-flex align-items-center justify-content-center my-4 gap-3">
-                                        <input type="file" class="form-control">
-
+                                <h5 class="card-title fw-semibold">Ganti Password</h5>
+                                <p class="card-subtitle mb-4">Untuk ganti password silahkan di sini</p>
+                                <form method="POST" action="{{ route('ubah-password') }}">
+                                    @csrf
+                                    <div class="mb-4">
+                                        <label for="current_password" class="form-label fw-semibold">Password
+                                            Sebelumnya</label>
+                                        <input type="password" class="form-control" id="current_password"
+                                            name="current_password">
+                                        @error('current_password')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
                                     </div>
-                                    <p class="mb-0">Di bolehkan hanya JPG or PNG. Berukuran maksimal 5Mb</p>
-                                </div>
+                                    <div class="mb-4">
+                                        <label for="new_password" class="form-label fw-semibold">Password Baru</label>
+                                        <input type="password" class="form-control" id="new_password" name="new_password">
+                                        @error('new_password')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="">
+                                        <label for="new_password_confirmation" class="form-label fw-semibold">Konfirmasi
+                                            Password</label>
+                                        <input type="password" class="form-control" id="new_password_confirmation"
+                                            name="new_password_confirmation">
+                                        @error('new_password_confirmation')
+                                            <div class="text-danger">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="d-flex justify-content-center mt-4">
+                                        <button type="submit" class="btn btn-primary">Ubah Password</button>
+                                    </div>
+                                </form>
+
                             </div>
                         </div>
                     </div>
+
                     <div class="col-lg-6 d-flex align-items-stretch">
                         <div class="card w-100 position-relative overflow-hidden">
                             <div class="card-body p-4">
-                                <h5 class="card-title fw-semibold">Ganti Password</h5>
-                                <p class="card-subtitle mb-4">Untuk ganti password silahkan konfirmasi disini</p>
-                                <form>
-                                    <div class="mb-4">
-                                        <label for="exampleInputPassword1" class="form-label fw-semibold">Password
-                                            Lama</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1"
-                                            value="">
+                                <form action="" method="POST" enctype="multipart/form-data">
+                                    {{-- <form action="{{ route('update-profile') }}" method="POST"> --}}
+                                    @method('PUT')
+                                    @csrf
+                                    <h5 class="card-title fw-semibold">Ubah Prifile</h5>
+                                    <p class="card-subtitle mb-4">Ubah photo profile silahkan masukkan di sini</p>
+                                    <div class="text-center">
+                                        <img src="assets/dist/images/profile/user-1.jpg" alt=""
+                                            class="img-fluid rounded-circle" width="120" height="120">
+                                        <div class="d-flex align-items-center justify-content-center my-4 gap-3">
+                                            <input type="file" name="foto" class="form-control"
+                                                id="exampleInputResume">
+                                        </div>
+                                        <p class="mb-0">Dibolehkan JPG atau PNG. Ukuran maksimal photo 5Mb</p>
                                     </div>
-                                    <div class="mb-4">
-                                        <label for="exampleInputPassword1" class="form-label fw-semibold">Password
-                                            Baru</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1"
-                                            value="">
-                                    </div>
-                                    <div class="">
-                                        <label for="exampleInputPassword1" class="form-label fw-semibold">Konfirmasi
-                                            Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1"
-                                            value="">
-                                    </div>
-                                </form>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="row">
                     <div class="col-12">
                         <div class="card w-100 position-relative overflow-hidden mb-0">
                             <div class="card-body p-4">
                                 <h5 class="card-title fw-semibold">Detail Profile</h5>
                                 <p class="card-subtitle mb-4">Profile detail</p>
-                                <form>
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <div class="mb-4">
-                                                <label for="exampleInputName" class="form-label fw-semibold">Masukkan
-                                                    Nama</label>
-                                                <input type="text" class="form-control" id="exampleInputName"
-                                                    placeholder="Mathew Anderson">
-                                            </div>
-                                            <div class="mb-4">
-                                                <label for="exampleInputEmail" class="form-label fw-semibold">Email</label>
-                                                <input type="email" class="form-control" id="exampleInputEmail"
-                                                    placeholder="info@modernize.com">
-                                            </div>
-                                            <div class="mb-4">
-                                                <label for="exampleInputPhone" class="form-label fw-semibold">No Telpon</label>
-                                                <input type="text" class="form-control" id="exampleInputPhone"
-                                                    placeholder="+91 12345 65478">
-                                            </div>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="mb-4">
+                                            <label for="exampleInputName" class="form-label fw-semibold">Masukkan
+                                                Nama</label>
+                                            <input type="text" name="name" value="{{ $user->name }}"
+                                                class="form-control" id="exampleInputName" placeholder="Mathew Anderson">
                                         </div>
-                                        <div class="col-lg-6">
-                                            <div class="mb-4">
-                                                <label for="exampleInputAddress"
-                                                    class="form-label fw-semibold">Alamat</label>
-                                                <textarea class="form-control" id="exampleInputAddress"
-                                                    placeholder="Jalan Pegangsaan Timur Nomor 14, RT 04 RW 05 Kel. Senayan, Kec. Kebayoran Baru, Jakarta Selatan, 10450"></textarea>
-                                            </div>
-                                            <div class="mb-4">
-                                                <label for="exampleInputResume" class="form-label fw-semibold">Masukkan
-                                                    Lamaran</label>
-                                                <input type="file" class="form-control" id="exampleInputResume">
-                                            </div>
-                                            <div class="mb-4">
-                                                <label for="exampleInputCV" class="form-label fw-semibold">Masukkan
-                                                    CV</label>
-                                                <input type="file" class="form-control" id="exampleInputCV">
-                                            </div>
+                                        <div class="mb-4">
+                                            <label for="exampleInputEmail" class="form-label fw-semibold">Email</label>
+                                            <input type="email" value="{{ $user->email }}" name="email"
+                                                class="form-control" id="exampleInputEmail"
+                                                placeholder="info@modernize.com">
+                                        </div>
+                                        <div class="mb-4">
+                                            <label for="exampleInputPhone" class="form-label fw-semibold">No
+                                                Telpon</label>
+                                            <input type="text" name="no_telp" value="{{ $user->no_telp }}"
+                                                class="form-control" id="exampleInputPhone" placeholder="+91 12345 65478">
                                         </div>
                                     </div>
-                                    <div class="col-12">
-                                        <div class="d-flex align-items-center justify-content-end mt-4 gap-3">
-                                            <button class="btn btn-primary">Save</button>
-                                            <button class="btn btn-light-danger text-danger">Cancel</button>
+                                    <div class="col-lg-6">
+                                        <div class="mb-4">
+                                            <label for="exampleInputAddress" class="form-label fw-semibold">Alamat</label>
+                                            <textarea name="alamat" class="form-control" id="exampleInputAddress"
+                                                placeholder="Jalan Pegangsaan Timur Nomor 14, RT 04 RW 05 Kel. Senayan, Kec. Kebayoran Baru, Jakarta Selatan, 10450">{{ $user->alamat }}</textarea>
+                                        </div>
+                                        <div class="mb-4">
+                                            <label for="exampleInputResume" class="form-label fw-semibold">Masukkan
+                                                Lamaran</label>
+                                            <input type="file" name="lamaran" class="form-control"
+                                                id="exampleInputResume">
+                                        </div>
+                                        <div class="mb-4">
+                                            <label for="exampleInputCV" class="form-label fw-semibold">Masukkan
+                                                CV</label>
+                                            <input type="file" class="form-control" id="exampleInputCV">
                                         </div>
                                     </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="d-flex align-items-center justify-content-end mt-4 gap-3">
+                                        <button class="btn btn-primary">Save</button>
+                                        <button class="btn btn-light-danger text-danger">Cancel</button>
+                                    </div>
+                                </div>
                             </div>
-                            </form>
                         </div>
                     </div>
+                    </form>
                 </div>
+
             </div>
         </div>
 
 
-    </div>
     </div>
 @endsection
