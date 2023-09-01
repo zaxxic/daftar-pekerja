fe2e7e968555f338a4f790bdb700fc3a26097cb4
TrisqiTegarValenzi <trisqitegarv@gmail.com>
2023-09-01 09:03:17 +0700

    pembenaran halaman lowongan

---
+++ lang/id-lang.php
@@ -0,0 +1,17 @@
+// resources/lang/id-id/id-lang.php
+
+<?php
+return [
+    'January'   => 'Januari',
+    'February'  => 'Februari',
+    'March'     => 'Maret',
+    'April'     => 'April',
+    'May'       => 'Mei',
+    'June'      => 'Juni',
+    'July'      => 'Juli',
+    'August'    => 'Agustus',
+    'September' => 'September',
+    'October'   => 'Oktober',
+    'November'  => 'November',
+    'December'  => 'Desember',
+];
