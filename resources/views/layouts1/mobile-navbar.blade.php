dcc197573a6d580a1b267e8d324ce80dde413513
TrisqiTegarValenzi <trisqitegarv@gmail.com>
2023-09-06 21:38:43 +0700

    pembenaran

--- resources/views/layouts1/mobile-navbar.blade.php
+++ resources/views/layouts1/mobile-navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="mobilenavbar" aria-labelledby="offcanvasWithBothOptionsLabel">
   <nav class="sidebar-nav scroll-sidebar">
     <div class="offcanvas-header justify-content-between">
-      <img width="200" src="{{asset('assets/hummas.png')}}" alt="" class="img-fluid">
+      <img width="200" src="{{asset('assets/hummatech.png')}}" alt="" class="img-fluid">
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body profile-dropdown mobile-navbar" data-simplebar="" data-simplebar>
