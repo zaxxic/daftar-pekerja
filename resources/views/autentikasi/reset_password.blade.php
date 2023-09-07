342be8e025b1ff399abcf9c3722dd030d5420587
suryamaulana98 <129302343+suryamaulana98@users.noreply.github.com>
2023-09-07 11:41:32 +0700

    forget password dan reset dong

--- resources/views/autentikasi/reset_password.blade.php
+++ resources/views/autentikasi/reset_password.blade.php
@@ -17,27 +17,55 @@
     <link rel="shortcut icon" type="image/png" href="{{ asset('assets/logo.png') }}" />
     <!-- Core Css -->
     <link id="themeColors" rel="stylesheet" href="{{ asset('assets/dist/css/style.min.css') }}" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.all.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.min.css" rel="stylesheet">
 </head>
 
 <body style="background-color: #f6f9fc;">
-    @if(session('success'))
-    <script>
-        Swal.fire({
-            icon: 'success',
-            title: 'Sukses',
-            text: "{{ session('success') }}",
-        });
-    </script>
+    @if (session('success'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Sukses',
+                text: "{{ session('success') }}",
+            });
+        </script>
     @endif
     <div id="root" class="min-h-100vh d-flex flex-column bg-light">
+        <style>
+            /* Tampilan untuk layar yang lebih kecil dari 768px (misalnya, ponsel) */
+            @media (max-width: 767px) {
+                .gambar {
+                    display: none;
+                    /* Menyembunyikan gambar dengan class "gambar" */
+                }
+
+                .wadah {
+                    margin-top: 250px;
+                    /* Gaya CSS khusus untuk tampilan ponsel di sini */
+                }
+            }
+
+            /* Tampilan untuk layar yang lebih kecil dari atau sama dengan 768px (misalnya, tablet) */
+            @media (min-width: 768px) {
+                #ini {
+                    display: none;
+                }
+
+                .wadah {
+                    /* Gaya CSS khusus untuk tampilan tablet di sini */
+                }
+            }
+        </style>
         <div class="container mt-5 bg-light">
             <div class="row align-items-center bg-light">
                 <div class="col-lg-6">
-                    <img class="w-100" style="margin-left: -10%;" src="{{ asset('assets/loginn.svg') }}" alt="image">
+                    <img class="w-100 gambar d-none d-lg-block" id="ini" style="margin-left: -10%; "
+                        src="{{ asset('assets/loginn.svg') }}" alt="image">
                 </div>
                 <div class="col-lg-6 shadow bg-white p-5">
                     <div class="text-center mt-2">
@@ -55,25 +83,29 @@
 
                         <div class="mb-3">
                             <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Email"
+                                value="{{ old('email') }}" required>
                             @error('email')
-                            <div class="text-danger">{{ $message }}</div>
+                                <div class="text-danger">{{ $message }}</div>
                             @enderror
                         </div>
 
                         <div class="mb-3">
                             <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                            <input type="password" class="form-control" id="password" name="password"
+                                placeholder="Password" required>
                             @error('password')
-                            <div class="text-danger">{{ $message }}</div>
+                                <div class="text-danger">{{ $message }}</div>
                             @enderror
                         </div>
 
                         <div class="mb-3">
-                            <label for="password_confirmation" class="form-label">Konfirmasi Password <span class="text-danger">*</span></label>
-                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Konfirmasi Password" required>
+                            <label for="password_confirmation" class="form-label">Konfirmasi Password <span
+                                    class="text-danger">*</span></label>
+                            <input type="password" class="form-control" id="password_confirmation"
+                                name="password_confirmation" placeholder="Konfirmasi Password" required>
                             @error('password_confirmation')
-                            <div class="invalid-feedback">{{ $message }}</div>
+                                <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
 
