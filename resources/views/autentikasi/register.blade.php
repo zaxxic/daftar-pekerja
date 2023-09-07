274c307e7a7033ccd1aafe9228982f1f9682f56a
suryamaulana98 <129302343+suryamaulana98@users.noreply.github.com>
2023-09-07 10:14:39 +0700

    login register

--- resources/views/autentikasi/register.blade.php
+++ resources/views/autentikasi/register.blade.php
@@ -19,7 +19,9 @@
     <link rel="shortcut icon" type="image/png" href="{{ asset('assets/logo.png') }}" />
     <!-- Core Css -->
     <link id="themeColors" rel="stylesheet" href="{{ asset('assets/dist/css/style.min.css') }}" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.all.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.min.css" rel="stylesheet">
@@ -38,46 +40,60 @@
     </div> --}}
     <!--  Body Wrapper -->
     @if (session('success'))
-    {{-- <div class="alert alert-success" role="alert">
+        {{-- <div class="alert alert-success" role="alert">
         {{ session('success') }}
     </div> --}}
-    <script>
-        Swal.fire({
-            icon: 'success',
-            title: 'Sukses',
-            text: "{{ session('success') }}",
-        });
-    </script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
+        <script>
             Swal.fire({
                 icon: 'success',
                 title: 'Sukses',
-                text: 'Berhasil mereset password.',
+                text: "{{ session('success') }}",
             });
-        });
-    </script>
-    <style>
-        @media (max-width: 767px) {
-            .gambar {
-                display: none;
-                /* Menyembunyikan gambar dengan class "gambar" */
+        </script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Sukses',
+                    text: 'Berhasil mereset password.',
+                });
+            });
+        </script>
+        <style>
+            /* Tampilan untuk layar yang lebih kecil dari 768px (misalnya, ponsel) */
+            @media (max-width: 767px) {
+                .gambar {
+                    display: none;
+                    /* Menyembunyikan gambar dengan class "gambar" */
+                }
+
+                .wadah {
+                    margin-top: 250px;
+                    /* Gaya CSS khusus untuk tampilan ponsel di sini */
+                }
             }
 
-            .wadah {
-                margin-top: 250px;
+            /* Tampilan untuk layar yang lebih kecil dari atau sama dengan 768px (misalnya, tablet) */
+            @media (min-width: 768px) {
+                #ini {
+                    display: none;
+                }
+
+                .wadah {
+                    /* Gaya CSS khusus untuk tampilan tablet di sini */
+                }
             }
-        }
-    </style>
+        </style>
     @endif
     <div id="root" class="min-h-100vh d-flex flex-column bg-light">
 
         <div class="container bg-light">
             <div class="row align-items-center bg-light">
                 <div class="col-lg-6">
-                    <img class="w-100 gambar d-none d-md-block" style="margin-left: -10%; " src="{{ asset('assets/loginn.svg') }}" alt="image">
+                    <img class="w-100 gambar d-none d-lg-block" id="ini" style="margin-left: -10%; "
+                        src="{{ asset('assets/loginn.svg') }}" alt="image">
                 </div>
-                <div class="col-lg-6 col-md-4 col-12">
+                <div class="col-lg-6 col-md-12 col-12">
                     <div class="card shadow h-100 mt-5">
                         <div class="card-body h-100" style="height: 100%;">
                             <div class="text-center mt-2">
@@ -94,19 +110,26 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="mb-3">
-                                            <label for="email" class="form-label">Nama <span style="color: red;">*</span></label>
-                                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
+                                            <label for="email" class="form-label">Nama <span
+                                                    style="color: red;">*</span></label>
+                                            <input type="text" name="name"
+                                                class="form-control @error('name') is-invalid @enderror"
+                                                value="{{ old('name') }}">
                                             @error('name')
-                                            <span class="invalid-feedback">{{ $message }}</span>
+                                                <span class="invalid-feedback">{{ $message }}</span>
                                             @enderror
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="mb-3">
-                                            <label for="email" class="form-label">Email <span style="color: red;">*</span></label>
-                                            <input type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" id="email" name="email" aria-describedby="emailHelp" />
+                                            <label for="email" class="form-label">Email <span
+                                                    style="color: red;">*</span></label>
+                                            <input type="email"
+                                                class="form-control @error('email') is-invalid @enderror"
+                                                value="{{ old('email') }}" id="email" name="email"
+                                                aria-describedby="emailHelp" />
                                             @error('email')
-                                            <div class="invalid-feedback">{{ $message }}</div>
+                                                <div class="invalid-feedback">{{ $message }}</div>
                                             @enderror
                                         </div>
                                     </div>
@@ -114,35 +137,47 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="mb-3">
-                                            <label for="exampleInputEmail1" class="form-label">Telephone <span style="color: red;">*</span></label>
-                                            <input type="number" class="form-control @error('no_telp') is-invalid @enderror" value="{{ old('no_telp') }}" id="no_telp" name="no_telp" aria-describedby="emailHelp" />
+                                            <label for="exampleInputEmail1" class="form-label">Telephone <span
+                                                    style="color: red;">*</span></label>
+                                            <input type="number"
+                                                class="form-control @error('no_telp') is-invalid @enderror"
+                                                value="{{ old('no_telp') }}" id="no_telp" name="no_telp"
+                                                aria-describedby="emailHelp" />
                                             @error('no_telp')
-                                            <div class="invalid-feedback">{{ $message }}</div>
+                                                <div class="invalid-feedback">{{ $message }}</div>
                                             @enderror
                                         </div>
                                     </div>
                                     <div class="col-md-6">
-                                        <label for="exampleInputEmail1" class="form-label mb-3">Jenis Kelamin <span style="color: red;">*</span></label>
+                                        <label for="exampleInputEmail1" class="form-label mb-3">Jenis Kelamin <span
+                                                style="color: red;">*</span></label>
                                         <div class="radio-container">
-                                            <input class="text-sm me-1" type="radio" name="jenis_kelamin" id="radio1" value="laki-laki" {{ old('jenis_kelamin') == 'laki-laki' ? 'checked' : '' }}>
+                                            <input class="text-sm me-1" type="radio" name="jenis_kelamin"
+                                                id="radio1" value="laki-laki"
+                                                {{ old('jenis_kelamin') == 'laki-laki' ? 'checked' : '' }}>
                                             <label class="mb-0 me-3" for="radio1">Laki-laki</label>
-                                            <input class="text-sm me-1" type="radio" name="jenis_kelamin" id="radio2" value="perempuan" {{ old('jenis_kelamin') == 'perempuan' ? 'checked' : '' }}>
+                                            <input class="text-sm me-1" type="radio" name="jenis_kelamin"
+                                                id="radio2" value="perempuan"
+                                                {{ old('jenis_kelamin') == 'perempuan' ? 'checked' : '' }}>
                                             <label class="mb-0" for="radio2">Perempuan</label>
                                         </div>
                                         @error('jenis_kelamin')
-                                        <div class="text-danger">{{ $message }}</div>
+                                            <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
                                 </div>
-                                
+
 
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="mb-3">
-                                            <label for="exampleInputPassword1" class="form-label">Alamat <span style="color: red;">*</span></label>
-                                            <textarea name="alamat" id="" cols="2" rows="2" class="form-control @error('alamat') is-invalid @enderror" value="{{ old('alamat') }}" id="alamat" name="alamat">{{ old('alamat') }}</textarea>
+                                            <label for="exampleInputPassword1" class="form-label">Alamat <span
+                                                    style="color: red;">*</span></label>
+                                            <textarea name="alamat" id="" cols="2" rows="2"
+                                                class="form-control @error('alamat') is-invalid @enderror" value="{{ old('alamat') }}" id="alamat"
+                                                name="alamat">{{ old('alamat') }}</textarea>
                                             @error('alamat')
-                                            <div class="invalid-feedback">{{ $message }}</div>
+                                                <div class="invalid-feedback">{{ $message }}</div>
                                             @enderror
                                         </div>
                                     </div>
@@ -151,10 +186,13 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="mb-3">
-                                            <label for="exampleInputEmail1" class="form-label">Password <span style="color: red;">*</span></label></label>
-                                            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" aria-describedby="emailHelp" />
+                                            <label for="exampleInputEmail1" class="form-label">Password <span
+                                                    style="color: red;">*</span></label></label>
+                                            <input type="password"
+                                                class="form-control @error('password') is-invalid @enderror"
+                                                id="password" name="password" aria-describedby="emailHelp" />
                                             @error('password')
-                                            <div class="invalid-feedback">{{ $message }}</div>
+                                                <div class="invalid-feedback">{{ $message }}</div>
                                             @enderror
                                         </div>
                                     </div>
@@ -163,15 +201,19 @@
                                         <div class="mb-3">
                                             <label for="exampleInputEmail1" class="form-label">Konfirmasi Password
                                                 <span style="color: red;">*</span></label></label>
-                                            <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation" name="password_confirmation" aria-describedby="emailHelp" />
+                                            <input type="password"
+                                                class="form-control @error('password_confirmation') is-invalid @enderror"
+                                                id="password_confirmation" name="password_confirmation"
+                                                aria-describedby="emailHelp" />
                                             @error('password_confirmation')
-                                            <div class="invalid-feedback">{{ $message }}</div>
+                                                <div class="invalid-feedback">{{ $message }}</div>
                                             @enderror
                                         </div>
                                     </div>
                                 </div>
 
-                                <button type="submit" id="daftar" class="btn btn-primary w-100 py-2 mb-4 rounded-2">Daftar
+                                <button type="submit" id="daftar"
+                                    class="btn btn-primary w-100 py-2 mb-4 rounded-2">Daftar
                                     Sekarang</button>
                                 <div class="d-flex align-items-center">
                                     <p class="fs-4 mb-0 text-dark">
@@ -180,7 +222,7 @@
                                     <a class="text-primary fw-medium ms-2" href="{{ route('login') }}">login</a>
                                 </div>
                             </form>
-                           
+
                             <div class="mt-3 text-center">
 
                             </div>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,9 @@
     <link rel="shortcut icon" type="image/png" href="{{ asset('assets/logo.png') }}" />
     <!-- Core Css -->
     <link id="themeColors" rel="stylesheet" href="{{ asset('assets/dist/css/style.min.css') }}" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.all.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.min.css" rel="stylesheet">
@@ -38,46 +40,59 @@
     </div> --}}
     <!--  Body Wrapper -->
     @if (session('success'))
-    {{-- <div class="alert alert-success" role="alert">
+        {{-- <div class="alert alert-success" role="alert">
         {{ session('success') }}
     </div> --}}
-    <script>
-        Swal.fire({
-            icon: 'success',
-            title: 'Sukses',
-            text: "{{ session('success') }}",
-        });
-    </script>
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Sukses',
+                text: "{{ session('success') }}",
+            });
+        </script>
 
 
-    <style>
-        @media (max-width: 767px) {
-            .gambar {
-                display: none;
-                /* Menyembunyikan gambar dengan class "gambar" */
+        <style>
+            @media (min-width: 768px) {
+                .gambar {
+                    display: none;
+                    /* Menyembunyikan gambar dengan class "gambar" */
+                }
+
+                .wadah {
+                    margin-top: 250px;
+                }
             }
 
-            .wadah {
-                margin-top: 250px;
+            /* Tampilan untuk tampilan tablet (misalnya, lebar 768px) */
+            @media (max-width: 768px) {
+                .gambar {
+                    display: none;
+                    /* Menyembunyikan gambar dengan class "gambar" */
+                }
+
+                .wadah {
+                    margin-top: 250px;
+                }
             }
-        }
-    </style>
+        </style>
     @endif
-    @if(session('reset'))
-    <script>
-        Swal.fire({
-            icon: 'success',
-            title: 'Sukses',
-            text: "{{ session('reset') }}",
-        });
-    </script>
+    @if (session('reset'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Sukses',
+                text: "{{ session('reset') }}",
+            });
+        </script>
     @endif
     <div id="root" class="min-h-100vh d-flex flex-column bg-light">
 
         <div class="container mt-5 bg-light">
             <div class="row align-items-center bg-light">
                 <div class="col-lg-6">
-                    <img class="w-100 gambar d-none d-md-block" style="margin-left: -10%;" src="{{ asset('assets/loginn.svg') }}" alt="image">
+                    <img class="w-100 gambar d-none d-lg-block" style="margin-left: -10%;"
+                        src="{{ asset('assets/loginn.svg') }}" alt="image">
                 </div>
                 <div class="col-lg-6 shadow bg-white p-5">
                     <div class="text-center mt-2">
@@ -93,17 +108,19 @@
                         @csrf
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Email"
+                                required>
                         </div>
                         @error('email')
-                        <div class="text-danger">{{ $message }}</div>
+                            <div class="text-danger">{{ $message }}</div>
                         @enderror
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                            <input type="password" class="form-control" id="password" name="password"
+                                placeholder="Password" required>
                         </div>
                         @error('password')
-                        <div class="text-danger">{{ $message }}</div>
+                            <div class="text-danger">{{ $message }}</div>
                         @enderror
                         <div class="mb-4 form-check">
                             <a href="{{ route('password.request') }}" class="text-sm float-end">Lupa Password?</a>
