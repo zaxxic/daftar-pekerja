9adbf4da3334653d1f572b0c159d28a3e1374512
tamhar-1404 <127166391+tamhar-1404@users.noreply.github.com>
2023-09-11 12:24:03 +0700

    approval

--- database/migrations/2023_09_08_130658_add_coloum_lokasi_wawancara.php
+++ database/migrations/2023_09_08_130658_add_coloum_lokasi_wawancara.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('lokasi_wawancara')->nullable();
+            $table->text('lokasi_wawancara')->nullable();
         });
 
     }
